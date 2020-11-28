aa08dae36db78a801858088c42413cba30a277b1
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-11-28 17:06:18 +0300

    Revert "developed 2nd task"
    This reverts commit 988d7e351e0908e03889b25b3c4c5cfaf1839742.

--- 2/CargoTrain.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'train'
-
-class CargoTrain < Train
-  def initialize(num)
-    super
-    @type = "Cargo"
-  end
-end
--- 2/PassengerTrain.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'train'
-
-class PassengerTrain < Train
-  def initialize(num)
-    super
-    @type = "Passenger"
-  end
-end
