54d7f9f4b25edbfd3ac62aa01937688fa1b3a49a
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-11-28 17:05:46 +0300

    Revert "fixed"
    This reverts commit dda13e1f2d623afaf2ffd5a29840b892ebc8c03f.

--- 1/route.rb
+++ 1/route.rb
@@ -1,13 +1,13 @@
 class Route
   attr_reader :stations
 
-  def initialize(start,finish)
-    @stations = [start, finish]
-  end
+	def initialize(start,finish)
+  	@stations = [start, finish]
+	end
 
-  def add_station(station)
-    @stations.insert(-1, station)
-  end
+	def add_station(station)
+  	@stations.insert(-1, station)
+	end
 end
 
 #clear
--- 1/train.rb
+++ 1/train.rb
@@ -1,63 +1,56 @@
 class Train
-  attr_accessor :speed
-  attr_reader :type; :number; :wagons_count; :route; :current_station
+	attr_accessor :speed
+	attr_reader :type; :number; :wagons_count; :route; :current_station
 
-  def initialize(type, num, wagons)
-    @type = type
-    @number = num
-    @wagons_count = wagons
-    @speed = 0
-  end
+	def initialize(type, num, wagons)
+	  @type = type
+	  @number = num
+	  @wagons_count = wagons
+	  @speed = 0
+	end
+	
+	def set_route(route)
+	  @route = route
+	  @current_station = @route.stations[0]
+	  @current_station.add_train(self)
+	end
 
-  def set_route(route)
-    @route = route
-    @current_station = @route.stations[0]
-    @current_station.add_train(self)
-  end
+	def add_wagon
+	  @wagons_count += 1 if @speed == 0
+	end
 
-  def add_wagon
-    @wagons_count += 1 if @speed == 0
-  end
+	def delete_wagon
+	  if @speed == 0
+    	@wagons_count -= 1
+	  end
+	end
 
-  def delete_wagon
-    if @speed == 0
-      @wagons_count -= 1
-    end
-  end
+	def move_to_next_station
+	  return unless next_station
+	  @current_station.depart_train(self)
+	  @current_station = next_station
+	  @current_station.add_train(self)
+	end
 
-  def move_to_next_station
-    return unless next_station
-    @current_station.depart_train(self)
-    @current_station = next_station
-    @current_station.add_train(self)
-  end
+	def move
+	  @speed = 80
+	end
 
-  def move_to_previous_station
-    return unless previous_station
-    @current_station.depart_train(self)
-    @current_station = previous_station
-    @current_station.add_train(self)
-  end
+	def stop
+	  @speed = 0
+	end
 
-  def move
-    @speed = 80
-  end
+	def next_station
+	  if @route.stations.index(@current_station) != @route.stations.last
+	    @route.stations[@route.stations.index(@current_station)+1]
+	  end
+	end
 
-  def stop
-    @speed = 0
-  end
-
-  def next_station
-    if @route.stations.index(@current_station) != @route.stations.last
-      @route.stations[@route.stations.index(@current_station)+1]
-    end
-  end
-
-  def previous_station
-    if @current_station != @route.stations[0]
-      @route.stations[@route.stations.index(@current_station)-1]
-    end
-  end
+	def previous_station
+	  if @current_station != @route.stations[0]
+		  @route.stations[@route.stations.index(@current_station)-1]
+	  end
+	end
 end
 
 
